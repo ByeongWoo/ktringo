7bc8051ad3f99105aab4ee6aecbd6d98157b9aa3
ByeongWoo <83655479+ByeongWoo@users.noreply.github.com>
2023-10-04 00:51:56 +0900

    수정

--- html/js/design.js
+++ html/js/design.js
@@ -5,7 +5,7 @@ $(document).ready(function () {
 			var width = window.innerWidth;
 			if (width < 672) {
 				// mobile
-				if (!$('.event-header_wrap').hasClass('scroll')){
+				if (!$('.header_wrap').hasClass('scroll')){
 					$(".header_wrap").addClass("scroll");
 				}
 				$(".navigator").addClass("mobile");
